42428cd43389e2f644f0d81ad90a5944e7d16387
Eric Jiang <eyjiang18@gmail.com>
2022-04-28 16:25:30 -0400

    added deck model

---
+++ src/models/deck.js
@@ -0,0 +1,37 @@
+const SUITS = ["♦", "♣", "♥", "♠"];
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+export default class Deck {
+  constructor(cards = newDeck()) {
+    this.cards = cards;
+  }
+}
+
+class Cards {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+}
+
+function newDeck() {
+  return SUITS.flatMap((suit) => {
+    return VALUES.map((value) => {
+      return new Cards(suit, value);
+    });
+  });
+}
